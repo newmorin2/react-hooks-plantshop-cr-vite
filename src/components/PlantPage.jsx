a9ebd071f5394b07728a0d0519512fac2c62a88a
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-13 15:45:12 +0300

    plant page

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -3,12 +3,12 @@ import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, onAddPlant, search, setSearch }) {
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={plants} />
     </main>
   );
 }

eaebc97b72f26d8970b5bc0e30f874bae23ae7e0
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-13 15:44:27 +0300

    actual searching ignore other

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import NewPlantForm from "./NewPlantForm";
-import PlantList from "./PlantList";
-import Search from "./Search";
 
-function PlantPage({ plants, onAddPlant, search, setSearch }) {
+function Search({search,setSearch}) {
   return (
-    <main>
-      <NewPlantForm onAddPlant={onAddPlant} />
-
-      <Search search={search} setSearch={setSearch} />
-
-      <PlantList plants={plants} />
-    </main>
+    <div className="searchBar">
+      <label htmlFor="search">Search Plants</label>
+      <input
+      type="text"
+      id="search"
+      placeholder="Type a name to search..."
+      value={search}
+      onChange={(e) =>
+        setSearch(e.target.value)}
+        />
+    </div>
   );
 }
 
-export default PlantPage;
+export default Search;
